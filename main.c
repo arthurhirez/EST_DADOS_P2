6a417c8bdc5d26c8e284c9f2edf5906f115f986a
ArthurHR <arthurhr@usp.br>
2023-05-20 09:37:19 -0300

    adicionado hash mult e desaloca mem

--- main.c
+++ main.c
@@ -31,6 +31,17 @@ hashT create_table(unsigned size){
     return table;
 }
 
+void delete_table(hashT *table){
+    free(table->data_array);
+}
+
+void delete_strings(string **input_array, unsigned size){
+    for(int i = 0; i < size; i++){
+        free((*input_array)[i]);
+    }
+    free((*input_array));
+}
+
 
 unsigned converter(string s) {
    unsigned h = 0;
@@ -91,7 +102,6 @@ int insert_hash_div(hashT *table, string element, unsigned size, unsigned *n_col
 
 int search_hash_div(hashT *table, string element, unsigned size, unsigned *n_found){
     unsigned key = converter(element);
-
     for(unsigned i = 0; i < size; i++){
         unsigned key_hash = h_div(key, i, size);
         // printf("[%d]:\t%20s\t=\t%d -> %d\n", i, element, key, key_hash);
@@ -100,7 +110,7 @@ int search_hash_div(hashT *table, string element, unsigned size, unsigned *n_fou
             return -1;
         }
 
-        if(strcmp(table->data_array[key_hash], element) == 0){
+        if((strcmp(table->data_array[key_hash], element) == 0) || strcmp(table->data_array[key_hash], "#####") == 0){
             printf("O elemento %s foi encontrado com sucesso!!!\n", table->data_array[key_hash]);
             (*n_found)++;
             return 0;
@@ -116,6 +126,44 @@ int search_hash_div(hashT *table, string element, unsigned size, unsigned *n_fou
 unsigned h_mul(unsigned x, unsigned i, unsigned B){
     const double A = 0.6180;
     return  ((int) ((fmod(x * A, 1) * B) + i)) % B;
+}
+
+int insert_hash_mul(hashT *table, string element, unsigned size, unsigned *n_colision){
+    unsigned key = converter(element);
+
+    for(unsigned i = 0; i < size; i++){
+        unsigned key_hash = h_mul(key, i, size);
+        printf("[%d]:\t%20s\t=\t%d -> %d\n", i, element, key, key_hash);
+
+        if(table->data_array[key_hash] == NULL || strcmp(table->data_array[key_hash], "#####") == 0){
+            table->data_array[key_hash] = element;
+            printf("O elemento %s foi inserido com sucesso!!!\n", table->data_array[key_hash]);
+            return 0;
+        }
+        (*n_colision)++;
+    }
+    return 1;
+}
+
+
+int search_hash_mul(hashT *table, string element, unsigned size, unsigned *n_found){
+    unsigned key = converter(element);
+    for(unsigned i = 0; i < size; i++){
+        unsigned key_hash = h_mul(key, i, size);
+        // printf("[%d]:\t%20s\t=\t%d -> %d\n", i, element, key, key_hash);
+        if(table->data_array[key_hash] == NULL){
+            // printf("O elemento %s NAO EXISTE na tabela!!!\n", element);
+            return -1;
+        }
+
+        if((strcmp(table->data_array[key_hash], element) == 0) || strcmp(table->data_array[key_hash], "#####") == 0){
+            printf("O elemento %s foi encontrado com sucesso!!!\n", table->data_array[key_hash]);
+            (*n_found)++;
+            return 0;
+        }
+        
+    }
+    return 1;
 }
 
 /*
@@ -161,10 +209,7 @@ int main(int argc, char const *argv[])
     }
     double tempo_insercao_h_div = finaliza_tempo();
 
-
     printf("\nBUSCA:\n");
-
-
 
     // consulta dos dados na tabela hash usando hash por divisão
     inicia_tempo();
@@ -175,27 +220,30 @@ int main(int argc, char const *argv[])
 
     // limpa a tabela hash com hash por divisão
 
-
-
-
+    delete_table(&table);
+    
+    table = create_table(B);
     // cria tabela hash com hash por divisão
 
     // inserção dos dados na tabela hash usando hash por multiplicação
     inicia_tempo();
     for (int i = 0; i < N; i++) {
-        // inserir insercoes[i] na tabela hash
+        insert_hash_mul(&table, insercoes[i], B, &colisoes_h_mul);  
     }
     double tempo_insercao_h_mul = finaliza_tempo();
 
     // busca dos dados na tabela hash com hash por multiplicação
     inicia_tempo();
     for (int i = 0; i < M; i++) {
-        // buscar consultas[i] na tabela hash
+        search_hash_mul(&table, consultas[i], B, &encontrados_h_mul);
     }
     double tempo_busca_h_mul = finaliza_tempo();
 
     // limpa a tabela hash com hash por multiplicação
+    delete_table(&table);
 
+    delete_strings(&insercoes, N);
+    delete_strings(&consultas, M);
 
 
     printf("Hash por Divisão\n");
